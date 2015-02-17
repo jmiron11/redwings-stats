43cb3774a82923d0bc18f720e5ce4c04f697c226
Justin Miron <justinmiron11@gmail.com>
2015-02-17 02:49:38 -0600

    added webscraping for seasons

---
+++ soupExtract.py
@@ -0,0 +1,97 @@
+from datetime import datetime, date, time
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+# formats the date, returns a string in form YYYY-MM-DD
+# unformatted string is a date in the form "Day Month Day, Year"
+def formatDate(unformatted):
+	date = datetime.strptime(unformatted, "%a %b %d, %Y")
+	date = date.strftime("%Y-%m-%d")
+	return date
+
+# returns the soup from an html address
+def htmlToSoup(address):
+	req = requests.get(address)
+	text = req.text
+	soup = BeautifulSoup(text)
+	return soup
+
+#returns a list of lists that holds schedule data
+def schedule(soup):
+	# dates contains all of the days the detroit red wings play on
+	schedule_data = []
+
+	# access the schedule table from the schedule_html above and get elements
+	schedule_table = soup.find('table', class_='data', border='0', cellspacing='0', cellpadding='1')
+	for row in schedule_table.find_all("tr"):
+		cells = row.find_all("td")
+		cells = [element.text.strip() for element in cells]
+		if len(cells) >= 5 and cells[0] != 'Date':
+			schedule_data.append([element for element in cells if element])
+
+	# data now holds the table information
+	# format data into proper lists for the below insert into database
+	for list_ in schedule_data:
+		while len(list_) < 6:
+			list_.append(None)
+		for index, ele in enumerate(list_): 
+			if index == 0:									# the date is stored in index 0
+				list_[index] = formatDate(ele)
+			elif index == 4: 								# set up the fourth index a
+				if ele != None and len(ele.split()) >= 6: 	# extend number of elements to 6 in each list
+					if ") OT" in ele: 						# check for shootout, overtime, or neither
+						list_[5] = "OT"
+					elif ") SO" in ele:
+						list_[5] = "SO"
+					else:
+						list_[5] = "NONE"
+
+					score = re.findall(r'\d+', ele) 		# find the number score using regex
+					if (len(score) != 2):
+						list_[4] = "NA"
+						list_[5] = "NONE"
+					elif (score[0] > score[1]):
+						list_[4] = "A" 						# store results as either home or away win
+					else:
+						list_[4] = "H"
+				else:
+					list_[4] = "NA"
+					list_[5] = "NONE"
+
+	return schedule_data
+
+def seasonPlayerData(soup):
+	season_player_data = []
+
+	data_table = soup.find('table', class_='data', cellpadding='0', cellspacing='0')
+	for row in data_table.find_all("tr"):
+		cells = row.find_all("td")
+		cells = [element.text for element in cells]
+		if cells[0] != '#':
+			season_player_data.append([element for element in cells if element])
+
+
+	return season_player_data
+
+
+
+def playerGameData(soup):
+	# webscrape for the html's of boxscores and store in a list
+	# store in list
+	address_list = []
+
+	data_table = soup.find_all('td', class_='leftAlignedColumn')
+	data_table = [element.a.get('href') for element in data_table]
+	address_list.append([element for element in data_table if element])
+	
+	# change recap to boxscore in each of the data_table elements
+	address_list = address_list[0]
+	for element in address_list:
+		element = element.replace("recap", "boxscore")
+
+	# webscrape those webpages for player game data
+
+
+	return #SOME JUNK
--- webscrape.py
+++ webscrape.py
@@ -1,88 +1,56 @@
-import os.path
 import sqlite3
 from datetime import datetime, date, time
-import re
-import requests
 from bs4 import BeautifulSoup
 
-# unformatted string is a date in the form "Day Month Day, Year"
-def formatDate(unformatted):
-	date = datetime.strptime(unformatted, "%a %b %d, %Y")
-	date = date.strftime("%Y-%m-%d")
-	return date
+import soupExtract
 
-# get the soup for the website
-schedule_html = "http://redwings.nhl.com/club/schedule.htm"
-schedule_req = requests.get(schedule_html)
-schedule_text = schedule_req.text
-schedule_soup = BeautifulSoup(schedule_text)
+schedule_soup = soupExtract.htmlToSoup("http://redwings.nhl.com/club/schedule.htm")
+schedule_data = soupExtract.schedule(schedule_soup)
 
-# dates contains all of the days the detroit red wings play on
-schedule_data = []
+season_player_soup = soupExtract.htmlToSoup("http://redwings.nhl.com/club/stats.htm?season=20142015%20-%2014/15%20season%20player%20stats")
+season_player_data = soupExtract.seasonPlayerData(season_player_soup)
 
-# access the schedule table from the schedule_html above and get elements
-table = schedule_soup.find('table', class_='data', border='0', cellspacing='0', cellpadding='1')
-for row in table.find_all("tr"):
-	cells = row.find_all("td")
-	cells = [element.text.strip() for element in cells]
-	if len(cells) >= 5 and cells[0] != 'Date':
-		schedule_data.append([element for element in cells if element])
+prev_season_player_soup = soupExtract.htmlToSoup("http://redwings.nhl.com/club/stats.htm?season=20132014")
+prev_season_player_data = soupExtract.seasonPlayerData(prev_season_player_soup)
 
-# data now holds the table information
-# format data into proper lists for the below insert into database
+player_game_soup = soupExtract.htmlToSoup("http://redwings.nhl.com/club/gamelog.htm")
+player_game_data = soupExtract.playerGameData(player_game_soup)
 
-for list_ in schedule_data:
-	while len(list_) < 6:
-		list_.append(None)
-	for index, ele in enumerate(list_): 
-		if index == 0:									# the date is stored in index 0
-			list_[index] = formatDate(ele)
-		elif index == 4: 								# set up the fourth index a
-			if ele != None and len(ele.split()) >= 6: 	# extend number of elements to 6 in each list
-				if ") OT" in ele: 						# check for shootout, overtime, or neither
-					list_[5] = "OT"
-				elif ") SO" in ele:
-					list_[5] = "SO"
-				else:
-					list_[5] = "NONE"
+# schedule_data holds the red wings schedule information to be placed in schedule table
+# team_game_data holds the red wings statistics per game to be placed in team_game table
+# player_data holds the red wings player statistics current season statistics
 
-				score = re.findall(r'\d+', ele) 		# find the number score using regex
-				if (len(score) != 2):
-					list_[4] = "NA"
-					list_[5] = "NONE"
-				elif (score[0] > score[1]):
-					list_[4] = "A" 						# store results as either home or away win
-				else:
-					list_[4] = "H"
-			else:
-				list_[4] = "NA"
-				list_[5] = "NONE"
+for list_ in player_game_data:
+	print(list_)
 
-#schedule_data holds the red wings schedule information to be placed in schedule table
-#team_game_data holds the red wings statistics per game to be placed in team_game table
-#player_data holds the red wings player statistics current season statistics
+# conn = sqlite3.connect('redwings.db')  					# close the database
+# c = conn.cursor()
 
-conn = sqlite3.connect('redwings.db')  					# close the database
-c = conn.cursor()
+# # create a table command
+# # date will be in the form "YYYY:MM:DD"
+# # visitor will hold "(away team name)"
+# # home will hold "(home team)"
+# # time will hold time in form "H:MM (PM/AM)" in ET
+# # result will either hold H,A, or NA based on whether home/away has won, or NA if not played yet
+# # extra will hold whether it went to extra time, "NONE" , "OT", "SO" for respective cases
 
-#create a table command
-# date will be in the form "YYYY:MM:DD"
-# visitor will hold "(away team name)"
-# home will hold "(home team)"
-# time will hold time in form "H:MM (PM/AM)" in ET
-# result will either hold H,A, or NA based on whether home/away has won, or NA if not played yet
-# extra will hold whether it went to extra time, "NONE" , "OT", "SO" for respective cases
+# # if not (os.path.isfile("./schedule.db")):
+# c.execute('CREATE TABLE IF NOT EXISTS schedule(date TEXT, visitor TEXT, home TEXT, time TEXT, result TEXT, extra TEXT)')
+# for list_ in schedule_data :
+# 	c.execute('INSERT OR REPLACE INTO schedule VALUES (?,?,?,?,?,?)', list_)
 
-# if not (os.path.isfile("./schedule.db")):
-c.execute('CREATE TABLE IF NOT EXISTS schedule(date TEXT, visitor TEXT, home TEXT, time TEXT, result TEXT, extra TEXT)')
-	#add information to the schedule database
-for list_ in schedule_data :
-	c.execute('INSERT INTO schedule VALUES (?,?,?,?,?,?)', list_)
+# c.execute('CREATE TABLE IF NOT EXISTS season_player_stats(num INTEGER, pos TEXT, name TEXT, gp INTEGER, g INTEGER, a INTEGER, p INTEGER, plusminus INTEGER, pim INTEGER, pp INTEGER, sh INTEGER, gw INTEGER, s INTEGER, spercent INTEGER)')
+# for list_ in season_player_data:
+# 	c.execute('INSERT OR REPLACE INTO season_player_stats VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)', list_)
 
-	## add interface for replacing information and updating database whenever script is run.
+# c.execute('CREATE TABLE IF NOT EXISTS prev_season_player_stats(num INTEGER, pos TEXT, name TEXT, gp INTEGER, g INTEGER, a INTEGER, p INTEGER, plusminus INTEGER, pim INTEGER, pp INTEGER, sh INTEGER, gw INTEGER, s INTEGER, spercent INTEGER)')
+# for list_ in prev_season_player_data:
+# 	c.execute('INSERT OR IGNORE INTO prev_season_player_stats VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)', list_)
 
-conn.commit() 	# close the connection
-conn.close()
+# ## add interface for replacing information and updating database whenever script is run.
+
+# conn.commit() 	# close the connection
+# conn.close()
 
 
 
